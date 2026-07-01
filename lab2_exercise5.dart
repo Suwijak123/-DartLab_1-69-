d973892107d79210b3c4e66b689c6bd9a0027866
Suwijak123 <1suwijakt@gmail.com>
2026-07-01 11:27:11 +0700

    1

--- lab2_exercise5.dart
+++ lab2_exercise5.dart
@@ -12,4 +12,23 @@ void printGrade(int score){
   }else{
     print('Grade: F');
   }
+  print('---');
 }
+void main(){
+  printGrade(85);
+  printGrade(72);
+  printGrade(50);
+  printGrade(40);
+
+  print('Numbers 1 to 10:');
+  for (int i =1; i<=10; i++){
+    print(i);
+  }
+  print('---');
+
+  var fruits = ['Apple', 'Banana', 'Cherry'];
+  print('Fruits:');
+  for (var fruit in fruits){
+    print('- $fruit');
+   }
+  }
